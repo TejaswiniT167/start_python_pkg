310e6d6309763f324e5d3cea204a5ba6c27a6054
Tejaswini <tejaswini3477@gmail.com>
2024-01-19 21:13:51 +0530

    editing setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,11 @@
 from setuptools import setup, find_packages
 from typing import List
 
-HYPEN_E_DOT = '-e .'
-
-
-def get_requirement(file_path):
-    requirements = []
-    with open(file_path) as f:
-        requirements = f.readlines()
-        requirements = [req.replace("\n", "") for req in requirements]
-
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    return requirements
-
-
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 REPO_NAME = "start_python_pkg"
 PKG_NAME = "MongoDB_Connect"
 AUTHOR_USER_NAME = "TejaswiniT167"
@@ -39,5 +25,4 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires=get_requirement("./requirements_dev.txt"),
 )
