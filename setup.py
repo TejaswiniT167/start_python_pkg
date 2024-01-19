aa6c76523670a42d609fce57add58a42483498d0
Tejaswini <tejaswini3477@gmail.com>
2024-01-19 20:29:29 +0530

    editing experiment file

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
 
-__version__ = "0.0.4"
+__version__ = "0.0.3"
 REPO_NAME = "start_python_pkg"
 PKG_NAME = "MongoDB_Connect"
 AUTHOR_USER_NAME = "TejaswiniT167"
